61a2159f3ba37b7dab6a41de30df47cd2635d816
Ayush Kumar <146207142+ayushkzz@users.noreply.github.com>
2026-01-30 10:18:15 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -9,17 +9,17 @@ public class BinarySearch {
         return mid;
       }
       if (arr[mid] < key) { 
-        start = mid - 1;
+        start = mid + 1;
       }
       else {
-        end = mid-1;
+        end = mid - 1;
       }
     }
     return -1; 
   }
   public static void main(String args[]) {
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    int key = 4;
+    int key = 6;
     if (binarySearch(arr, key) != -1) {
       System.out.println("Key Found at index : " + binarySearch(arr, key));
     }
